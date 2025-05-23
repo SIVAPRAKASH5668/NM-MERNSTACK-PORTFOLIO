6688de17e30fb6270a97f882501124411fd95b9a
SIVAPRAKASH-S <144685762+SIVAPRAKASH5668@users.noreply.github.com>
2025-05-24 01:48:05 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NAAN-MUDALVAN-PROJECT/', // 🔧 Your repo name
+  base: '/', // 🔧 Your repo name
   plugins: [react()]
 })
